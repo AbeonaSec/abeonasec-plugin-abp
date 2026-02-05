d67634ebb9663850b9cb414b99e300005ccd4e49
Aaron Krapes <aaronkrapes@outlook.com>
2026-02-05 10:09:54 -0700

    Added barebones file structure

---
+++ data_run.py
@@ -0,0 +1,9 @@
+# data_run.py
+# data collection runtime for Anomolous Behavior Profiling plugin for the AbeonaSec application
+# written by Aaron Krapes
+# Jan 26, 2026
+
+# imports
+# scapy for sniffing packets
+# http post packets to pipeline address
+
---
+++ pipe_run.py
@@ -0,0 +1,9 @@
+# pipe_run.py
+# intializer and pipeline runtime for Anomolous Behavior Profiling plugin for the AbeonaSec application
+# written by Aaron Krapes
+# Jan 26, 2026
+
+# download and add abp-pcap model to Triton Server
+# imports
+# create pipeline and stages
+# run pipeline
