f9c8ca4eb8531971c5e9ec3c8b736ee4cf37611a
Aaron Krapes <aaronkrapes@outlook.com>
2026-02-06 22:01:44 -0700

    Added basic, untested shell script for initializing the components of the ABP plugin.

--- init.sh
+++ init.sh
@@ -4,15 +4,92 @@
 # script to intialize and run the 
 # Anomolous Behavior Profiling plugin
 # written by Aaron Krapes
-# Feb 5, 2026
+# Feb 6, 2026
+
+# model handling logic
+# maybe copy from this folder to a specified one?
+# or are we restarting tritonserver?
 
 # check that pip is installed
-# install python dependencies -- error handling
-# set port number for HTTP input stage (check for open?)
-# get name of network interface
-ip route | grep default | awk '{print $5}' | awk /./
-# prompt with LEGAL disclaimer about sniffing on network interface
+# if not, install
+if ! command -v pip &> /dev/null; then
+    apt-get update
+    apt-get install -y python3-pip
+    # if apt-get failed exit and error
+    if [ $? -ne 0 ]; then
+        echo "CRITICAL: Failed to install pip using apt-get."
+        echo "Recommend installing manually and re-running this script." 
+        exit 1
+    fi
+fi
+
+# install python dependencies
+# NOTE: add error handling
+pip install -r requirements.txt
+apt-get install -y libpcap-dev
+
+# set port number for HTTP input stage
+# loop from 8003 to 8079 and find the first unused port
+PORT=8003
+MAX=8079
+while true; do
+    # netcat the port to see if it is being used
+    if ! nc -z localhost "$PORT" 2>/dev/null; then
+        break
+    # if it is, iterate the value and repeat
+    else
+        PORT=$((PORT + 1))
+        # if maxxed out at 8079, error and quit
+        if [ "$PORT" -gt "$MAX" ]; then
+            echo "CRITICAL: All localhost ports between $START and $MAX are in use."
+            echo "Read docs to manually choose a port and run the Morpheus pipeline."
+            exit 1
+        fi
+    fi
+done
+echo "Using port $PORT for Morpheus pipeline input..."
+
+# legal disclaimer about sniffing on network interface
+echo "
+LEGAL DISCLAIMER 
+-----------------------------------------------------------------------
+THIS PROGRAM SNIFFS PACKETS FROM A SPECIFIED NETWORK INTERFACE
+
+DO NOT EVER RUN THIS PROGRAM GIVEN AN INTERFACE THAT HAS ACCESS TO A 
+NETWORK WHICH YOU DO NOT OWN OR HAVE LEGAL PERMISSION TO ADMINISTRATE
+-----------------------------------------------------------------------
+"
+
+# prompt the user to acknowledge
+read -p "Type 'accept' to acknowledge and proceed with the installation: " INPUT
+if [ "$INPUT" != "accept" ]; then
+    exit 1
+fi
+
+# get name of default network interface
+NET_IF=$(ip route | grep default | awk '{print $5}' | awk /./)
+
 # ask user if _^^ network interface is the one they would like to run the plugin on
-# run python files -- error handling
-# NOTE: command line arguments for port number and network interface
-# copy model to models folder and remove (?)
+read -p "Is $NET_IF the Network Interface that you would like to sniff on? (y/N): " INPUT
+if [ "$INPUT" == "N" ]; then
+    read -p "Input the Network Interface that you would like to sniff on: " NET_IF
+elif [ "$INPUT" != "y"]; then
+    exit 1 # temporary exit with invalid input
+fi
+
+# run python files
+# NOTE: add error handling
+chmod +x data_run.py pipe_run.py
+./pipe_run.py $PORT
+
+# make sure http server input stage is running before starting the sniffing code
+# setting timeout of 60 seconds for now (can adjust later if problematic)
+TIMEOUT=60
+if timeout "$TIMEOUT" nc -z localhost "$PORT" > /dev/null 2>&1; then
+    ./data_run.py $PORT $NET_IF
+    echo "Started sniffing on $NET_IF. Triple check that this is the correct interface." 
+else
+    echo "CRITICAL: Morpheus pipeline has not started after 60 seconds."
+    echo "Ensure pipe_run.py is started and the Morpheus HTTP Server is listening on $PORT."
+    exit 1
+fi
