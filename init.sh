2459c536cac85a8ae2835d46450d34c4b503c0f4
Aaron Krapes <aaronkrapes@outlook.com>
2026-02-09 16:10:15 -0700

    Updated plugin structure and initialization process to accomodate containerization, including a basic compose.

--- init.sh
+++ init.sh
@@ -10,44 +10,28 @@
 # maybe copy from this folder to a specified one?
 # or are we restarting tritonserver?
 
-# check that pip is installed
-# if not, install
-if ! command -v pip &> /dev/null; then
-    apt-get update
-    apt-get install -y python3-pip
-    # if apt-get failed exit and error
-    if [ $? -ne 0 ]; then
-        echo "CRITICAL: Failed to install pip using apt-get."
-        echo "Recommend installing manually and re-running this script." 
-        exit 1
-    fi
-fi
-
-# install python dependencies
-# NOTE: add error handling
-pip install -r requirements.txt
-apt-get install -y libpcap-dev
-
 # set port number for HTTP input stage
 # loop from 8003 to 8079 and find the first unused port
 PORT=8003
 MAX=8079
 while true; do
-    # netcat the port to see if it is being used
-    if ! nc -z localhost "$PORT" 2>/dev/null; then
+    # run ss inside ui container (most stable) to see if any given port is being used
+    if ! podman exec abeonasec-ui ss -tulpn | grep -q ":$PORT" 2>/dev/null; then
         break
     # if it is, iterate the value and repeat
     else
         PORT=$((PORT + 1))
         # if maxxed out at 8079, error and quit
         if [ "$PORT" -gt "$MAX" ]; then
-            echo "CRITICAL: All localhost ports between $START and $MAX are in use."
+            echo "CRITICAL: All podman pod ports between $START and $MAX are in use."
             echo "Read docs to manually choose a port and run the Morpheus pipeline."
             exit 1
         fi
     fi
 done
 echo "Using port $PORT for Morpheus pipeline input..."
+touch .env
+echo "PIPE_IN_PORT=$PORT" >> .env
 
 # legal disclaimer about sniffing on network interface
 echo "
@@ -78,25 +62,12 @@ elif [ "$INPUT" != "y" ]; then
     echo "Ensure that you know what interface to be sniffing on"
     exit 1
 fi
+echo "Creating bridge with interface $NET_IF..."
 
-# run python files
-# NOTE: add error handling
-chmod +x data_run.py pipe_run.py
-./pipe_run.py $PORT
+# create podman network to bridge this interface into the container
+BRIDGE=plugin-abp-bridge
+podman network create -d macvlan -o parent=$NET_IF plugin-abp-bridge
+echo "NET_IF=$BRIDGE" >> .env
 
-# make sure http server input stage is running before starting the sniffing code
-# setting timeout of 60 seconds for now (can adjust later if problematic)
-TIMEOUT=60
-START_TIME=$SECONDS
-until nc -z localhost "$PORT" 2>/dev/null; do
-    if [ $(( SECONDS - START_TIME )) -ge $TIMEOUT ]; then
-        echo "CRITICAL: Morpheus pipeline has not started after 60 seconds."
-        echo "Ensure pipe_run.py is started and the Morpheus HTTP Server is listening on $PORT."
-        exit 1
-    fi
-    sleep 0.5
-done
-
-./data_run.py $PORT $NET_IF
-echo "Setup complete!!"
-echo "Started sniffing on $NET_IF. Triple check that this is the correct interface."
+# call podman compose to start building container
+podman compose up -d
