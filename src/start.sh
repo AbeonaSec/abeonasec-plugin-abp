7e1d1fd4d41a3cf9eb3277b553b60553443bfcbb
AaronATM <aaronkrapes@outlook.com>
2026-02-11 03:37:22 +0000

    updated docker compose with testing ports and proper permissions for the container. Minor tweaks to start.sh for plugin container launch

--- src/start.sh
+++ src/start.sh
@@ -6,24 +6,37 @@
 # written by Aaron Krapes
 # Feb 10, 2026
 
+echo "
+LEGAL DISCLAIMER 
+--------------------------------------------------------------------------------
+THIS PROGRAM SNIFFS PACKETS FROM A SPECIFIED NETWORK INTERFACE
+
+DO NOT EVER RUN THIS PROGRAM GIVEN AN INTERFACE THAT HAS ACCESS TO A 
+NETWORK WHICH YOU DO NOT OWN OR HAVE LEGAL PERMISSION TO ADMINISTRATE
+
+THE DEVELOPERS OF ABEONASEC TAKE NO RESPONSIBILITY FOR MISUSE OF THE APPLICATION
+--------------------------------------------------------------------------------
+"
+
 echo "Checking Morpheus pipeline environment variable..."
 if [[ -z "$ABP_PIPE_PORT" ]]; then
     echo "CRITICAL: Morpheus pipeline port has not been passed to the container."
     echo "Please set the environment variable ABP_PIPE_PORT."
-    kill -s SIGTERM 1 
+    exit 1
 fi
 echo "Found value: $ABP_PIPE_PORT."
 
 echo "Checking if Morpheus pipeline has been started..."
 TIMEOUT=60
 START_TIME=$SECONDS
-until ss -tulpn | grep -q ":${ABP_PIPE_PORT}" 2>/dev/null; do
+until nc -z -w 3 "host.containers.internal" "$ABP_PIPE_PORT"; do
     if [ $(( SECONDS - START_TIME )) -ge $TIMEOUT ]; then
         echo "CRITICAL: Morpheus pipeline has not started after 60 seconds."
         echo "Ensure pipe_run.py is started and the Morpheus HTTP Server is listening on $ABP_PIPE_PORT."
-        kill -s SIGTERM 1
+        exit 1
     fi
     sleep 0.5
 done
-python data_run.py ${ABP_PIPE_PORT}
-echo "Setup complete, started sniffing."
+echo "Checking container network interface."
+ip addr | grep -A 3 "eth0"
+python3 data_run.py $ABP_PIPE_PORT
